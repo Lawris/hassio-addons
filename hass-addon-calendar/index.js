dffc60c83518672cebc0800a83d91738d5f61167
Loris Plasson Simoni <>
2022-05-14 17:23:33 +0200

    fffff

--- hass-addon-calendar/index.js
+++ hass-addon-calendar/index.js
@@ -358,6 +358,22 @@ async function postEvents(sensorName) {
   }
   catch (error) {
     console.log("postEvents error with: " + error);
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request);
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
   }
 }
 
